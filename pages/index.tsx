462cd96e5af393fd14f03dff73d059f8c9f2b39f
pensjonatus <pensjonatus@gmail.com>
2022-07-03 18:12:41 +0200

    more stylish styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,45 @@ import Link from "next/link";
 import Layout from "../components/layout";
 import styles from "../styles/dom.module.css";
 
+type Feature = {
+  title: string;
+  description: React.ReactNode;
+  link: string;
+};
+
+const featured: Feature[] = [
+  {
+    title: "Rozważna konsumpcja",
+    description: (
+      <>
+        Interaktywny plakat pomagający konsumentowi nie zagubić się pośród
+        wolnej woli nadanej przez prawa wolnego rynku.
+      </>
+    ),
+    link: "/sensible",
+  },
+];
+
+const pageDescription = "Strona artysty Paweł Kowaluk";
+
 export default function Home() {
   return (
-    <Layout title="Dom" description="Strona artysty Paweł Kowaluk">
+    <Layout title="Dom" description={pageDescription}>
       <div className={styles.outer}>
         <div className={styles.inner}>
           <div className={styles.header}>
             <h1 className={styles.title}>kwlk</h1>
-            <p>Strona artysty Paweł Kowaluk</p>
+            <p>{pageDescription}</p>
           </div>
-          <Link href="/sensible">
-            <a>Rozważna konsumpcja</a>
-          </Link>
+          {featured.map(({ title, description, link }, key) => (
+            <section key={key} className={styles.feature}>
+              <h2>{title}</h2>
+              <p>{description}</p>
+              <Link href={link}>
+                <a className={styles.featureLink}>Zobacz</a>
+              </Link>
+            </section>
+          ))}
         </div>
       </div>
     </Layout>
