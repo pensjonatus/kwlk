a44d1cd7b4f09998f77a7a08399b6231bab8fedf
pensjonatus <pensjonatus@gmail.com>
2022-07-03 17:57:32 +0200

    style updates

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import { ReactNode } from "react";
 import styles from "./layout.module.css";
 
@@ -16,10 +18,29 @@ export default function Layout({ title, description, children }: LayoutProps) {
         <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className={styles.header}>
+        <Link href="/">
+          <a>
+            <Image
+              src="/kwlk.png"
+              alt="Link do strony domowej"
+              width={32}
+              height={32}
+            />
+          </a>
+        </Link>
+      </header>
 
       <main className={styles.main}>{children}</main>
 
-      <footer className={styles.footer}></footer>
+      <footer className={styles.footer}>
+        <Link href="https://github.com/pensjonatus/kwlk">
+          <a>Github</a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/pawel-kowaluk/">
+          <a>Autor</a>
+        </Link>
+      </footer>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import Layout from "../components/layout";
+import styles from "../styles/dom.module.css";
 
 export default function Home() {
   return (
     <Layout title="Dom" description="Strona artysty Paweł Kowaluk">
-      <Link href="/sensible">
-        <a>Rozważna konsumpcja</a>
-      </Link>
+      <div className={styles.outer}>
+        <div className={styles.inner}>
+          <div className={styles.header}>
+            <h1 className={styles.title}>kwlk</h1>
+            <p>Strona artysty Paweł Kowaluk</p>
+          </div>
+          <Link href="/sensible">
+            <a>Rozważna konsumpcja</a>
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 }
