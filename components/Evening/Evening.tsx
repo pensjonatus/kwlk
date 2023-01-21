03bd16e1c0db55b0fbcd21720b56ec8dea39f74c
Paweł Kowaluk <pensjonatus@gmail.com>
2023-01-21 12:32:58 +0100

    add text

--- components/Evening/Evening.tsx
+++ components/Evening/Evening.tsx
@@ -1,16 +1,34 @@
-import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import eveningImage from "./evening.png";
 
 export default function Evening() {
+  const [showText, setShowText] = useState(false);
+
+  useEffect(function () {
+    setTimeout(() => {
+      setShowText(true);
+    }, 1000 * 60 * 12);
+  }, []);
+
   return (
-    <Box display="flex" justifyContent="center" paddingTop={6}>
+    <Stack alignItems="center" paddingTop={6}>
       <Image
         width={507}
         height={731}
         src={eveningImage}
         alt="Wieczorem, postać wygląda z okna na tle ciepłego światła. Nie widzimy jej twarzy, ale jesteśmy spokojni. To nie potwór."
+        priority
       />
-    </Box>
+      <Typography
+        component="div"
+        color="white"
+        sx={{ textShadow: "#FC0 1px 0 10px", opacity: showText ? 1 : 0 }}
+      >
+        Pomimo wojny nuklearnej, niewiele się zmieniło.
+      </Typography>
+    </Stack>
   );
 }
