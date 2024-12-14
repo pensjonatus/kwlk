136912ec118fa9e26c115c43a9340928ec346f7b
Paweł Kowaluk <pensjonatus@gmail.com>
2024-12-14 16:40:18 +0100

    update the section header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -34,7 +34,7 @@ export default function Index() {
         </header>
         <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
           <p className="leading-6 text-gray-700 dark:text-gray-200">
-            What&apos;s next?
+            What do you expect?
           </p>
           <ul>
             {resources.map(({ href, text }) => (
