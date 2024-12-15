aab36167579ccba5d94ead1341f2246e37a9d7e5
Paweł Kowaluk <pensjonatus@gmail.com>
2024-12-15 19:03:41 +0100

    set without buttons

--- app/components/Retirement/DateSelector.tsx
+++ app/components/Retirement/DateSelector.tsx
@@ -1,35 +1,19 @@
-import { useState } from "react";
-
 type DateSelectorProps = {
   setDate: (newDate: Date) => void;
 };
 
 export function DateSelector({ setDate }: DateSelectorProps) {
-  const [dateString, setDateString] = useState("");
   const id = "retirement-date-selector";
 
   return (
-    <form
-      className="flex flex-col gap-3 mt-8 mx-auto py-4 px-10 dark:bg-slate-900 bg-slate-400 rounded-lg"
-      onSubmit={(event) => {
-        event.preventDefault();
-        setDate(new Date(dateString));
-      }}
-    >
+    <form className="flex flex-col gap-3 mt-8 mx-auto py-4 px-10 dark:bg-slate-900 bg-slate-400 rounded-lg">
       <label htmlFor={id}>When do you want to retire?</label>
       <input
         id={id}
         type="date"
-        value={dateString}
         className="px-6 py-4"
-        onChange={(event) => setDateString(event.target.value)}
+        onChange={(event) => setDate(new Date(event.target.value))}
       />
-      <button
-        type="submit"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Set
-      </button>
     </form>
   );
 }
